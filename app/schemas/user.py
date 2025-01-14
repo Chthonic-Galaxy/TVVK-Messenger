3d817336481b8693a9ee527e2da968bd91620ab3
Chthonic-Galaxy <162060169+Chthonic-Galaxy@users.noreply.github.com>
2025-01-14 11:12:41 -0300

    created: User's schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,19 @@
+from typing import Optional, Annotated
+
+from pydantic import BaseModel, EmailStr, StringConstraints, ConfigDict
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: Annotated[str, StringConstraints(min_length=8)]
+    username: Annotated[str, StringConstraints(min_length=3, max_length=50)]
+    nickname: Optional[Annotated[str, StringConstraints(max_length=50)]] = None
+
+
+class UserRead(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    email: EmailStr
+    username: str
+    nickname: Optional[str] = None
