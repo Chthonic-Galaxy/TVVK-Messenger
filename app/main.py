88e11c14ca7aee6680f48ff66c47752487c3e1c6
Chthonic-Galaxy <162060169+Chthonic-Galaxy@users.noreply.github.com>
2025-01-18 01:10:01 -0300

    chore: Update router in app/main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from app.api.endpoints import auth, users
+from app.api.endpoints import auth, users, contacts
 from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
@@ -18,3 +18,4 @@ if settings.BACKEND_CORS_ORIGINS:
 
 app.include_router(auth.router, prefix=f"{settings.API_V1_STR}/auth", tags=["authentication"])
 app.include_router(users.router, prefix=f"{settings.API_V1_STR}/users", tags=["users"])
+app.include_router(contacts.router, prefix=f"{settings.API_V1_STR}/contacts", tags=["contacts"])
